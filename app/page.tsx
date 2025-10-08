b149636694e6bedd899fe85308db11005329c6d7
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-10-08 09:45:21 +0300

    Update Hero styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Hero } from "@/components/hero";
+import { Header } from "@/components/header";
 
 const Home = () => {
   return (
-    <main className="w-full max-w-480 mx-auto">
+    <main className="w-full max-w-480 mx-auto overflow-hidden bg-white">
+      <Header />
       <Hero />
       <div className="h-1000" />
     </main>
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,27 +1,41 @@
-import { IoIosArrowRoundForward } from "react-icons/io";
+"use client";
+
+import { motion } from "framer-motion";
+import { ArrowRight } from "lucide-react";
+
+import { fadeIn, heroContainerVariant } from "@/lib/variants";
 
 export const Hero = () => {
   return (
-    <section className="relative min-h-[50vh] lg:h-237 overflow-hidden bg-hero bg-cover bg-center bg-no-repeat">
-      <div className="container min-h-[40vh] lg:h-full flex items-center justify-center xl:justify-end">
+    <section className="relative mt-30 lg:mt-37.5 min-h-[40vh] lg:h-237 bg-hero bg-cover bg-center bg-no-repeat">
+      <motion.div
+        variants={heroContainerVariant}
+        initial="hidden"
+        whileInView={"show"}
+        className="container min-h-[40vh] lg:h-full flex items-center justify-center xl:justify-end"
+      >
         <div className="lg:max-w-160 text-white text-center lg:text-left">
-          <h1 className="h1">I’m Void Sir.</h1>
-          <p className="mb-8 lg:mb-16 max-w-lg leading-relaxed">
+          <motion.h1 variants={fadeIn("down")} className="h1">
+            I’m Void Sir.
+          </motion.h1>
+          <motion.p
+            variants={fadeIn("down")}
+            className="mb-8 lg:mb-16 max-w-lg leading-relaxed"
+          >
             Tattoos have their own unique power and magic. They not only
             beautify the body but also the psyche.
-          </p>
-          <div>
-            <button
-              type="button"
-              className="btn btn-sm lg:btn-lg btn-outline mx-auto lg:mx-0"
-            >
-              read more
-              <IoIosArrowRoundForward size={20} />
-            </button>
-          </div>
+          </motion.p>
+          <motion.button
+            variants={fadeIn("down")}
+            type="button"
+            className="btn btn-sm lg:btn-lg btn-outline mx-auto lg:mx-0"
+          >
+            read more
+            <ArrowRight size={20} />
+          </motion.button>
         </div>
         <div className="hidden xl:flex absolute bottom-48 right-2 left-0 before:absolute before:inset-0 before:bg-outlineText before:bg-no-repeat before:bg-center before:bg-contain before:h-50" />
-      </div>
+      </motion.div>
     </section>
   );
 };
